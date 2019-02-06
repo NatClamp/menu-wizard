7066bd68e8e6a81612aff7919b6e7964bcfb4cd3
NatClamp <nat.clamp@gmail.com>
2019-02-06 20:26:42 +0000

    general basic styling set-up and renaming allrecipes component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Router, navigate } from '@reach/router';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import RecipesList from './components/RecipesList';
+import AllRecipes from './components/AllRecipes';
 import AddRecipe from './components/AddRecipe';
 import SingleRecipe from './components/SingleRecipe';
 
@@ -14,7 +14,7 @@ class App extends Component {
         <Header />
 
         <Router>
-          <RecipesList path="/" />
+          <AllRecipes path="/" />
           <SingleRecipe path="/recipes/:recipe_id" />
           <AddRecipe path="/add-recipe" />
         </Router>
