71167d12073ed71f6999c81a9b0fa34ecd60c794
NatClamp <nat.clamp@gmail.com>
2019-02-08 13:22:29 +0000

    adding images to recipe cards and using flexbox to arrange

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from '@reach/router';
 import './Cards.css';
+import food from '../img/corn.jpg';
 
 const Cards = props => {
   const { recipes } = props;
@@ -8,20 +10,32 @@ const Cards = props => {
       {recipes.map(recipe => {
         return (
           <article className="card" key={recipe.id}>
-            <div className="card_title">{recipe.name}</div>
-            <div className="card_description">{recipe.description}</div>
-            <div className="card_cookingTime">
-              Cooking Time: {recipe.cookingTime} minutes
-            </div>
-            <div className="card_tags">
-              {recipe.tags.map(tag => {
-                return (
-                  <p key={tag} className="card_tags_single">
-                    {tag}
-                  </p>
-                );
-              })}
-            </div>
+            <section className="card_imageContainer">
+              {/* <img src={`${recipe.image}`} alt="Recipe Image" /> */}
+              <Link to={`/recipes/${recipe.id}`}>
+                <img src={food} alt="food" className="card_image" />
+              </Link>
+            </section>
+            <section className="card_infoContainer">
+              <div className="card_title">
+                <Link to={`/recipes/${recipe.id}`} className="navLink">
+                  {recipe.name}
+                </Link>
+              </div>
+              <div className="card_description">{recipe.description}</div>
+              <div className="card_cookingTime">
+                Cooking Time: {recipe.cookingTime} minutes
+              </div>
+              <div className="card_tags">
+                {recipe.tags.map(tag => {
+                  return (
+                    <p key={tag} className="card_tags_single">
+                      {tag}
+                    </p>
+                  );
+                })}
+              </div>
+            </section>
           </article>
         );
       })}
