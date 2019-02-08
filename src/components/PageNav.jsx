a77494cb6feb344f9e81791c0eba4cf3932cc611
NatClamp <nat.clamp@gmail.com>
2019-02-08 12:27:51 +0000

    Adding basic logic for styling current page navigation buttons on AllRecipes

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './PageNav.css';
 
-const PageNav = props => {
-  const { currentPage, pageNums, handlePageChange } = props;
-  return (
-    <div>
-      {pageNums.map(num => {
-        return (
-          <button key={num} id={num} onClick={handlePageChange}>
-            {num}
-          </button>
-        );
-      })}
-    </div>
-  );
-};
+class PageNav extends Component {
+  render() {
+    console.log('rendering...');
+    const { currentPage, pageNums, handlePageChange } = this.props;
+
+    return (
+      <nav className="pageNav">
+        {pageNums.map(num => {
+          return (
+            <button
+              key={num}
+              id={num}
+              onClick={handlePageChange}
+              className={
+                num === Number(currentPage)
+                  ? 'pageNav__button pageNav__button--current'
+                  : 'pageNav__button'
+              }
+            >
+              {num}
+            </button>
+          );
+        })}
+      </nav>
+    );
+  }
+}
 
 export default PageNav;
